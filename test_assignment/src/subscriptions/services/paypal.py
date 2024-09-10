e43ca01e58446cb4c8da634a4c82e59ac1af6687
Toma <greebsasha24@gmail.com>
2024-09-10 21:06:34 +0300

    feat: Add new func update_subscription_paypal.

--- test_assignment/src/subscriptions/services/paypal.py
+++ test_assignment/src/subscriptions/services/paypal.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+from http import HTTPStatus
 import json
 from typing import Dict
 
@@ -33,3 +34,48 @@ def cancel_subscription_paypal(access_token, subscription_id: str) -> None:
     url: str = f'https://api.sandbox.paypal.com/v1/billing/subscriptions/{subscription_id}/cancel'
 
     response = requests.post(url, headers=headers)
+
+
+def update_subscription_paypal(access_token, subscription_id: str) -> str:
+    bearer_token: str = f'Bearer {access_token}'
+
+    headers: Dict[str, str] = {
+        'Content-Type': 'application',
+        'Authorization': bearer_token,
+    }
+
+    subscriptions_details = Subscription.objects.get(paypal_subscription_id=subscription_id)
+    current_subscription_plan = subscriptions_details.subscription_plan
+
+    if current_subscription_plan == 'Basic':
+        new_subscription_plan_id: str = 'P-95719217972434046M3OEGAA'
+
+    elif current_subscription_plan == 'Premium':
+        new_subscription_plan_id: str = 'P-8E708017MM2102055M3OD57Y'
+
+    url: str = f'https://api.sandbox.paypal.com/v1/billing/subscriptions/{subscription_id}/revise'
+
+    revision_data: Dict[str, str] = {
+        'plan_id': new_subscription_plan_id,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(revision_data))
+
+    response_data = response.json()
+
+    approval_url = None
+
+    for link in response_data.get['links', []]:
+        if link.get('rel') == 'approve':
+            approval_url = link['href']
+
+    if response.status_code == HTTPStatus.OK:
+        return approval_url
+    else:
+        return None
+
+
+
+
+
+
