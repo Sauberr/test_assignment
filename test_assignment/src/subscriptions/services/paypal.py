ef70d0376323ba156135ff504e8642d3f2df37c2
Toma <greebsasha24@gmail.com>
2024-09-15 20:06:29 +0300

    feat: Add update_subscription_paypal, get_current_subscription.

--- test_assignment/src/subscriptions/services/paypal.py
+++ test_assignment/src/subscriptions/services/paypal.py
@@ -36,8 +36,9 @@ def cancel_subscription_paypal(access_token, subscription_id: str) -> None:
     response = requests.post(url, headers=headers)
 
 
-def update_subscription_paypal(access_token, subscription_id: str) -> str:
+def update_subscription_paypal(access_token, subscription_id: str) -> str | None:
     bearer_token: str = f'Bearer {access_token}'
+
 
     headers: Dict[str, str] = {
         'Content-Type': 'application',
@@ -46,12 +47,25 @@ def update_subscription_paypal(access_token, subscription_id: str) -> str:
 
     subscriptions_details = Subscription.objects.get(paypal_subscription_id=subscription_id)
     current_subscription_plan = subscriptions_details.subscription_plan
+    new_plan = None
 
     if current_subscription_plan == 'Basic':
-        new_subscription_plan_id: str = 'P-95719217972434046M3OEGAA'
+        if new_plan == 'Premium':
+            new_subscription_plan_id: str = 'P-95719217972434046M3OEGAA'
+        elif new_plan == 'Enterprise':
+            new_subscription_plan_id: str = 'P-5VS486129B569981AM3OEJXY'
 
-    elif current_subscription_plan == 'Premium':
-        new_subscription_plan_id: str = 'P-8E708017MM2102055M3OD57Y'
+    if current_subscription_plan == 'Premium':
+        if new_plan == 'Basic':
+            new_subscription_plan_id: str = 'P-8E708017MM2102055M3OD57Y'
+        elif new_plan == 'Enterprise':
+            new_subscription_plan_id: str = 'P-5VS486129B569981AM3OEJXY'
+
+    if current_subscription_plan == 'Enterprise':
+        if new_plan == 'Basic':
+            new_subscription_plan_id: str = 'P-8E708017MM2102055M3OD57Y'
+        elif new_plan == 'Premium':
+            new_subscription_plan_id: str = 'P-95719217972434046M3OEGAA'
 
     url: str = f'https://api.sandbox.paypal.com/v1/billing/subscriptions/{subscription_id}/revise'
 
@@ -71,11 +85,29 @@ def update_subscription_paypal(access_token, subscription_id: str) -> str:
 
     if response.status_code == HTTPStatus.OK:
         return approval_url
+
     else:
         return None
 
 
+def get_current_subscription(access_token, subscription_id: str) -> str | None:
+    bearer_token: str = f'Bearer {access_token}'
 
+    headers: Dict[str, str] = {
+        'Content-Type': 'application/json',
+        'Authorization': bearer_token,
+    }
 
+    url: str = f'https://apisandbox.paypal.com/v1/billing/subscriptions/{subscription_id}'
 
+    response = requests.get(url, headers=headers)
 
+    if response.status_code == HTTPStatus.OK:
+        subscription_data = response.json()
+
+        current_plan_id = subscription_data.get('plan_id')
+
+        return current_plan_id
+
+    else:
+        return None
