e690920de4e6d59c10072937be71cb0d6e870ce7
Toma <greebsasha24@gmail.com>
2024-09-15 20:08:06 +0300

    feat: Add create, delete, update, django_update_subscription_confirmed, paypal_update_subscription_confirmed, confirm_delete_subscription template.

--- test_assignment/src/subscriptions/urls.py
+++ test_assignment/src/subscriptions/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from subscriptions.views import create_subscription, delete_subscription, update_subscription
+from subscriptions.views import (create_subscription, delete_subscription, update_subscription,
+                                 paypal_update_subscription_confirmed, django_update_subscription_confirmed,
+                                 confirm_delete_subscription)
 
 
-app_name: str = "subscriptions"
+app_name: str = 'subscriptions'
 
 urlpatterns = [
     path('create-subscription/<str:subscription_id>/<str:plan>/', create_subscription, name='create_subscription'),
-    path('delete-subscription/<str:subscription_id>/', delete_subscription, name='delete_subscription'),
-    path('update-subscription/<str:subscription_id>/', delete_subscription, name='update_subscription'),
+    path('delete-subscription/<str:subscription_id>/', confirm_delete_subscription, name='confirm_delete_subscription'),
+    path('delete-subscription-confirmed/<str:subscription_id>/', delete_subscription, name='delete_subscription'),
+    path('update-subscription/<str:subscription_id>/', update_subscription, name='update_subscription'),
+
+    path('paypal-update-subscription-confirmed/', paypal_update_subscription_confirmed,
+         name='paypal_update_subscription_confirmed'),
+    path('django-update-subscription-confirmed/<str:subscription_id>/', django_update_subscription_confirmed,
+         name='django_update_subscription_confirmed'),
 ]
--- test_assignment/src/subscriptions/views.py
+++ test_assignment/src/subscriptions/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 from subscriptions.models import Subscription
 
-from subscriptions.services.paypal import get_access_token, cancel_subscription_paypal
+from subscriptions.services.paypal import get_access_token, cancel_subscription_paypal, update_subscription_paypal, \
+    get_current_subscription
 
 
 def create_subscription(request, subscription_id, plan):
@@ -13,7 +15,7 @@ def create_subscription(request, subscription_id, plan):
 
     selected_subscription_plan = plan
 
-    if selected_subscription_plan == "Basic":
+    if selected_subscription_plan == 'Basic':
         subscriptions_cost = "4.99"
     elif selected_subscription_plan == "Premium":
         subscriptions_cost = "9.99"
@@ -34,6 +36,11 @@ def create_subscription(request, subscription_id, plan):
     return render(request, 'subscriptions/create_subscription.html', context)
 
 
+def confirm_delete_subscription(request, subscription_id):
+    context = {'subscription_id': subscription_id}
+    return render(request, 'subscriptions/confirm_delete_subscription.html', context)
+
+
 def delete_subscription(request, subscription_id):
     access_token = get_access_token()
     cancel_subscription_paypal(access_token, subscription_id)
@@ -44,5 +51,63 @@ def delete_subscription(request, subscription_id):
     return render(request, 'subscriptions/delete_subscription.html')
 
 
-def update_subscription(request, subscription_id):
-    ...
+def update_subscription(request, subscription_id: str):
+    access_token = get_access_token()
+    approve_link = update_subscription_paypal(access_token, subscription_id)
+    if approve_link:
+        return redirect(approve_link)
+    else:
+        return HttpResponse("Unable to obtain the approval link. Please try again later.")
+
+
+def paypal_update_subscription_confirmed(request):
+    try:
+        subscription_details = Subscription.objects.get(user=request.user)
+        subscription_id = subscription_details.paypal_subscription_id
+
+        context = {'subscription_id': subscription_id}
+
+        return render(request, 'subscriptions/paypal_update_subscription_confirmed.html', context)
+
+    except:
+
+        return render(request, 'subscriptions/paypal_update_subscription_confirmed.html')
+
+
+def django_update_subscription_confirmed(request, subscription_id: str):
+    access_token = get_access_token()
+    current_plan_id = get_current_subscription(access_token, subscription_id)
+
+    if current_plan_id == 'P-8E708017MM2102055M3OD57Y':
+        new_plan_name: str = 'Basic'
+        new_plan_cost: str = '4.99'
+
+        Subscription.objects.filter(paypal_subscription_id=subscription_id).update(
+            subscription_plan=new_plan_name,  subscription_cost=new_plan_cost
+        )
+
+    elif current_plan_id == 'P-95719217972434046M3OEGAA':
+        new_plan_name: str = 'Premium'
+        new_plan_cost: str = '9.99'
+
+        Subscription.objects.filter(paypal_subscription_id=subscription_id).update(
+            subscription_plan=new_plan_name,  subscription_cost=new_plan_cost
+        )
+
+    elif current_plan_id == 'P-5VS486129B569981AM3OEJXY':
+        new_plan_name: str = 'Enterprise'
+        new_plan_cost: str = '14.99'
+
+        Subscription.objects.filter(paypal_subscription_id=subscription_id).update(
+            subscription_plan=new_plan_name,  subscription_cost=new_plan_cost
+        )
+
+    return render(request, 'subscriptions/django_update_subscription_confirmed.html')
+
+
+
+
+
+
+
+    
