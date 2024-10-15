2f6ef13dfe74da4017e35c79ad87dc62d7bfcd8f
Toma <greebsasha24@gmail.com>
2024-10-15 20:21:30 +0300

    Add: Migrations

--- test_assignment/src/subscriptions/urls.py
+++ test_assignment/src/subscriptions/urls.py
@@ -7,11 +7,14 @@ from subscriptions.views import (CreateSubscription, DeleteSubscription, UpdateS
 app_name: str = 'subscriptions'
 
 urlpatterns = [
+
     path('create-subscription/<str:subscription_id>/<str:plan>/', CreateSubscription.as_view(), name='create_subscription'),
+    path('update-subscription/<str:subscription_id>/<str:new_plan>/', UpdateSubscription.as_view(), name='update_subscription'),
+
     path('delete-subscription/<str:subscription_id>/', ConfirmDeleteSubscription.as_view(),
          name='confirm_delete_subscription'),
-    path('delete-subscription-confirmed/<str:subscription_id>/', DeleteSubscription.as_view(), name='delete_subscription'),
-    path('update-subscription/<str:subscription_id>/<str:new_plan>/', UpdateSubscription.as_view(), name='update_subscription'),
+    path('delete-subscription-confirmed/<str:subscription_id>/', DeleteSubscription.as_view(),
+         name='delete_subscription'),
 
     path('paypal-update-subscription-confirmed/', PaypalUpdateSubscriptionConfirmed.as_view(),
          name='paypal_update_subscription_confirmed'),
